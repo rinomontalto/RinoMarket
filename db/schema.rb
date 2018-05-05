d1c3b8298bd2111d935aa64a04e7f7c6e733e77c
rinomontalto <rinomontalto@tiscal.it>
2018-05-05 05:08:46 -0400

    aggiunto scaffold content

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180502165756) do
+ActiveRecord::Schema.define(version: 20180505090306) do
+
+  create_table "contents", force: :cascade do |t|
+    t.string "titolo"
+    t.text "descrizione"
+    t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
