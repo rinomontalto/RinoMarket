f648f914783f8aa2cbf2726d62d32b6cbfb6a334
rinomontalto <rinomontalto@tiscal.it>
2018-05-15 10:20:41 +0200

    modificata pagine index e show

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,6 +1,6 @@
 class Content < ApplicationRecord
 	belongs_to :user
-	has_attached_file :cover, styles: { medium: "300x>", thumb: "100x>" }
+	has_attached_file :cover
   	validates_attachment_content_type :cover, content_type: /\Aimage\/.*\z/,
   	message:"Supportate solo file immagini"
 
